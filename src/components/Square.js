3346224390f77b13c984a40e6fe7787117600ca6
Jay Villari <jay.villari12@gmail.com>
2020-11-19 17:37:04 -0500

    added textboxes to square that only allow 1-9

--- src/components/Square.js
+++ src/components/Square.js
@@ -4,9 +4,25 @@ class Square extends Component {
     render() {
         return (
             <div className="square">
-                
+                <input type="text" maxLength="1" onKeyPress={this.onKeyPress} onChange={this.onChange}/>
             </div>
         )
+    }
+
+    // Only allow numbers 1-9 to be entered into the box
+    onKeyPress= e =>  {
+        const keyCode = e.keyCode || e.which;
+        const keyValue = String.fromCharCode(keyCode);
+
+        // If the character entered is not 1-9, don't allow it 
+        if (!/^([1-9])$/.test(keyValue))
+          e.preventDefault();
+      }
+
+      // Log input value
+      onChange = e => {
+        let input = e.target.value;
+        console.log(input);
     }
 }
 
