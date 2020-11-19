a01a3d79cc4a4832e7936e9b5f0d3a9801fa2450
Jay Villari <jay.villari12@gmail.com>
2020-11-19 14:21:47 -0500

    added square component and updated Row.js to include it

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Square from './Square';
 
 class Row extends Component {
     render() {
         return (
             <div className="row">
                 Row here
+                <Square />
             </div>
         )
     }
