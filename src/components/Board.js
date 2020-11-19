1b97310d086e684430605ffab2375a1bad7bb6e2
Jay Villari <jay.villari12@gmail.com>
2020-11-19 14:17:39 -0500

    added row component and updated Board.js to include it

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Row from './Row';
 
 class Board extends Component {
     render() {
         return (
             <div className="board">
                 Board here
+                <Row />
             </div>
         )
     }
---
+++ src/components/Row.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Row extends Component {
+    render() {
+        return (
+            <div className="row">
+                Row here
+            </div>
+        )
+    }
+}
+
+export default Row;
