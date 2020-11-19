d8757a08c5bcbfa833e969121fe3d768f7c3ff08
Jay Villari <jay.villari12@gmail.com>
2020-11-19 14:10:06 -0500

    added board component and updated app.js to include it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
 import React, {Component} from 'react'
+import './App.css';
+
+import Board from './components/Board';
+
 class App extends Component{
   render(){
     return(
       <div className="App">
           <h1>Sudoku</h1>
+          <Board />
       </div>
     )
   }
