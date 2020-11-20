f353b3a204937ca6249a0683296eefc0099a870e
John Mieszkowski <jmm3@njit.edu>
2020-11-20 17:13:58 -0500

    Updated timer

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<h1>Sublocu</h1>
+				<h1 align="center">Sublocu</h1>
 
 				<Board
 					setDefaultBoard={this.setDefaultBoard}
--- src/components/Control.js
+++ src/components/Control.js
@@ -1,33 +1,53 @@
 import React, { Component } from "react";
 
-class Control extends Component{
-	
-    render() {
-        // const count = () => {
-        //     return "This Works";
-
-        // };
-        // document.getElementById("timer").innerHTML = "5";
-    
-
-	return (
-		<>
-			<div className="current">
-				Current game:
-				<button class="current restart">Restart</button>
-				<button class="current end">End</button>
-			</div>
-			<div className="new">
-				<span class="newtext">New game:</span>
-				<button class="new easy" >Easy</button>
-				<button class="new medium">Medium</button>
-				<button class="new hard">Hard</button>
-				<button class="new random">Random</button>
-			</div>
-			<p id="timer"></p>
-		</>
-    );
+class Control extends Component {
+	constructor() {
+		super();
+		this.state = {
+			countStart: 0,
+		};
+		this.counter = this.counter.bind(this);
+		this.count = this.count.bind(this);
+	}
+	// componentDidMount(){
+	//     this.count();
+	// }
+	counter = () => {
+		this.setState({ countStart: this.state.countStart + 1 });
+	};
+	count = () => {
+		setInterval(() => {
+			this.counter();
+		}, 1000);
     }
-};
+    
+    componentWillUnmount(){
+        clearInterval(this.countStart)
+    }
+
+	render() {
+		return (
+			<>
+				<div className="current">
+					Current game:
+					<button className="current restart" onClick={this.count}>
+						Restart
+					</button>
+					<button className="current end" onClick={clearInterval(this.countStart)}>
+						End
+					</button>
+				</div>
+				<div className="new">
+					<span className="newtext">New game:</span>
+					<button className="new easy">Easy</button>
+					<button className="new medium">Medium</button>
+					<button className="new hard">Hard</button>
+					<button className="new random">Random</button>
+				</div>
+				Timer: {this.state.countStart}
+			</>
+		);
+	}
+}
 
 export default Control;
