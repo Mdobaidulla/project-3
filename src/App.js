485678e5a757da12cd16a8cba56cc964746f4f17
Jay Villari <jay.villari12@gmail.com>
2020-11-21 20:26:14 -0500

    refactor transform board to function for solution board

--- src/App.js
+++ src/App.js
@@ -21,14 +21,34 @@ class App extends Component {
 	 * @param {this is the value we will get from API get call} boardValue
 	 */
 	setDefaultBoard = (boardValue, response) => {
+    let transformBoard =[];
+    // transform board values so each array represents a box instead of a row
+    for(let i = 0; i < 9; i=i+3)
+    {   
+      transformBoard[i] = boardValue[i].slice(0,3).concat(boardValue[i+1].slice(0,3),boardValue[i+2].slice(0,3));
+      transformBoard[i+1] = boardValue[i].slice(3,6).concat(boardValue[i+1].slice(3,6),boardValue[i+2].slice(3,6));
+      transformBoard[i+2] = boardValue[i].slice(6,9).concat(boardValue[i+1].slice(6,9),boardValue[i+2].slice(6,9)); 
+    }
+
 		this.setState({
-			defaultBoard: boardValue,
+			defaultBoard: transformBoard,
 			board: response,
 		});
 	};
 	//This method is updating the value that we hve from solition board
 	setSolutionBoard = (solutionBoard) => {
-		this.setState({ solutionBoard });
+    let transformSolution = [];
+    // transform solution board so each array represents a box instead of a row
+     for(let i = 0; i < 9; i=i+3)
+     {    
+        transformSolution[i] = solutionBoard[i].slice(0,3).concat(solutionBoard[i+1].slice(0,3),solutionBoard[i+2].slice(0,3));
+        transformSolution[i+1] = solutionBoard[i].slice(3,6).concat(solutionBoard[i+1].slice(3,6),solutionBoard[i+2].slice(3,6));
+        transformSolution[i+2] = solutionBoard[i].slice(6,9).concat(solutionBoard[i+1].slice(6,9),solutionBoard[i+2].slice(6,9)); 
+     }
+
+		this.setState({ 
+      solutionBoard: transformSolution 
+    });
 	};
 
 	render() {
